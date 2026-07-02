8615ebce1509dddd8bd2df3ae22b2bf5bced11de
Marvin <marvincampos0987@gmail.com>
2026-07-01 20:05:52 -0600

    feat: add tslib library and capacitor preferences dependency
    - Added tslib.js and tslib.html files to the project.
    - Updated package.json to include @capacitor/preferences dependency.
    - Created package-lock.json to lock dependencies.

---
+++ Lab3KeyronYMarvin/clasificacionLigas-app/src/app/services/clasification.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
+import { environment } from 'src/environments/environment.prod';
+import { ILeague } from '../models/league.model';
+import { from } from 'rxjs/internal/observable/from';
+import { ISeason } from '../models/season.model';
+import { IClasification } from '../models/clasification.model';
+import { CODE_LEAGUES } from '../constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClasificationService {
+
+  getFootballLeagues(){
+
+    return from(CapacitorHttp.get({
+      url: `${environment.apiURL}/all_leagues.php`
+    }).then( (response: HttpResponse) =>{
+
+      const leagues = response.data['leagues'] as ILeague[];
+      if(!leagues){
+        return [];
+      }
+      return leagues
+          .filter(league => CODE_LEAGUES.includes(league.idLeague))
+          .sort( (a, b) => a.strLeagueAlternate < b.strLeagueAlternate ? -1 : 1 );
+    }).catch( (error)=> {
+      return [];
+    }))
+
+  }
+
+  getSeasons(idLeague: string){
+
+    return from(CapacitorHttp.get({
+      url: `${environment.apiURL}/search_all_seasons.php?id=${idLeague}`
+    }).then( (response: HttpResponse)=>{
+      const seasons = response.data['seasons'] as ISeason[];
+      if(!seasons){
+        return [];
+      }
+      return seasons.reverse();
+    }).catch( (error)=> {
+      return [];
+    }))
+
+  }
+
+  getTableClasification(idLeague: string, season: string){
+
+    return from(CapacitorHttp.get({
+      url: `${environment.apiURL}/lookuptable.php?l=${idLeague}&s=${season}`
+    }).then( (response: HttpResponse) => {
+      const clasification = response.data['table'] as IClasification[];
+      if(!clasification){
+        return [];
+      }
+      return clasification;
+    }).catch( (error)=> {
+      return [];
+    }))
+
+  }
+
+}
