ce7744cd660ec29661d506314a76b2f0182c9f58
marvincampos0987 <marvincampos0987@gmail.com>
2026-07-01 20:53:20 -0600

    Delete Lab2Grupo6 directory

--- Lab2Grupo6/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Lab2Grupo6',
-  webDir: 'www'
-};
-
-export default config;
