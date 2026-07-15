8fb1289f1a1108d44c91a6277e33783a9da65f35
Marvin <marvincampos0987@gmail.com>
2026-07-15 17:13:16 -0600

    Corregir nombre de .gitignore

---
+++ Lab3KeyronYMarvin/clasificacionLigas-app/android/app/build/intermediates/assets/debug/public/chunk-5CYI27V2.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-B7E77EFB.js";import"./chunk-2JERCKEL.js";import"./chunk-TU24IGQZ.js";import"./chunk-LZZW4XTH.js";import"./chunk-NMFL75IO.js";import"./chunk-4CLCTAJ7.js";export{a as mdTransitionAnimation};
---
+++ Lab3KeyronYMarvin/clasificacionLigas-app/android/app/build/intermediates/assets/debug/public/chunk-NQCIUMMI.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-DMGLQ2EG.js";import"./chunk-2JERCKEL.js";import"./chunk-TU24IGQZ.js";import"./chunk-LZZW4XTH.js";import"./chunk-NMFL75IO.js";import"./chunk-4CLCTAJ7.js";export{b as iosTransitionAnimation,a as shadow};
